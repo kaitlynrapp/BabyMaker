f289561acb3214208ad4c264df9787d634cb8436
kaitlyn.rapp <kaitlyn.rapp@MU-C1-11.mullen.pvt.k12.co.us>
2021-09-22 11:53:41 -0600

    commit with set and gets

--- BabyMaker/src/Baby.java
+++ BabyMaker/src/Baby.java
@@ -1,10 +1,40 @@
 
 public class Baby
 	{
-String name;
-double weight;
-boolean isGirl;
+private String name;
+private double weight;
+private boolean isGirl;
 		
+
+public String getName()
+	{
+		return name;
+	}
+
+public void setName(String name)
+	{
+		this.name = name;
+	}
+
+public double getWeight()
+	{
+		return weight;
+	}
+
+public void setWeight(double weight)
+	{
+		this.weight = weight;
+	}
+
+public boolean isGirl()
+	{
+		return isGirl;
+	}
+
+public void setGirl(boolean isGirl)
+	{
+		this.isGirl = isGirl;
+	}
 
 public Baby(String n, double w, boolean g)
 {
--- BabyMaker/src/BabyMaker.java
+++ BabyMaker/src/BabyMaker.java
@@ -6,10 +6,13 @@ public class BabyMaker
 				Baby sarasBaby = new Baby("Juniper", 42.6, true);
 				Baby myBaby = new Baby("Malcom", 7.5, false);
 				
-				System.out.println("Kaitlyn's first baby was " + myBaby.isGirl);
-				System.out.println("Sara's first baby was " + sarasBaby.weight + ".");
+				System.out.println("Kaitlyn's first baby was " + myBaby.isGirl());
+				System.out.println("Sara's first baby was " + sarasBaby.getWeight() + ".");
 				
-				if (sarasBaby.weight > 10)
+				myBaby.setName("Maive");
+				System.out.println("Kaitlyn's first baby's name is " + myBaby.getName());
+				
+				if (sarasBaby.getWeight() > 10)
 					{
 						sarasBaby.eat();
 						System.out.println("Thats a fat baby.");
